09739c58105a5bb0e66f9a0eef85157c07c5754d
Jess Hottenstein <jess.hottenstein@gmail.com>
2012-12-02 23:31:26 -0500

    Removing extra scripts
    * Using jake to run tests, don't need scripts

--- scripts/run_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Running Testacular Server (http://vojtajina.github.com/testacular)"
-echo "-------------------------------------------------------------------"
-
-node_modules/testacular/bin/testacular run
