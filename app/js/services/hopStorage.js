30f9542721affb46ca75ac5342c82f682be97443
Jess Hottenstein <jess.hottenstein@gmail.com>
2013-04-10 23:14:15 -0400

    Persisting from hopStorage to localStorage

--- app/js/services/hopStorage.js
+++ app/js/services/hopStorage.js
@@ -3,5 +3,12 @@
 brewsheetApp.factory('hopStorage', function ($rootScope, localStorage) {
   var key = 'hopStorage';
   var hopStorageString = localStorage[key];
-  return {hopBill: undefined};
+
+  var hopBill = {hopBill: undefined};
+
+  $rootScope.$watch(function() { return hopBill; }, function() {
+    localStorage[key] = JSON.stringify(hopBill);
+  }, true);
+
+  return hopBill;
 });
--- test/unit/services/hopStorageSpec.js
+++ test/unit/services/hopStorageSpec.js
@@ -1,58 +1,57 @@
-/*jshint camelcase:false*/
+/*jshint camelcase:false */
+/*global HopBill:false */
 'use strict';
 
-describe('hop storage service', function() {
+describe('hop storage service', function () {
   var hopStorage, localStorage, $rootScope;
 
   beforeEach(module('brewsheetApp'));
 
-  describe('Empty Local Storage Tests', function() {
-    beforeEach(function() {
-      module(function($provide) {
+  describe('Empty Local Storage Tests', function () {
+    beforeEach(function () {
+      module(function ($provide) {
         localStorage = {};
 
         $provide.value('localStorage', localStorage);
       });
-      inject(function(_hopStorage_, _$rootScope_) {
+      inject(function (_hopStorage_, _$rootScope_) {
         hopStorage = _hopStorage_;
         $rootScope = _$rootScope_;
       });
     });
 
-    it('should default to undefined', function() {
+    it('should default to undefined', function () {
       expect(hopStorage.hopBill).toBe(undefined);
     });
-//
-//  it('should persist to localStorage', function() {
-//    $rootScope.$apply(function() {
-//      hopStorage.name = 'Jasper';
-//      hopStorage.style = 'Sarsaparilla';
-//      hopStorage.og = 1.020;
-//      hopStorage.ibu = 10;
-//      hopStorage.srm = 20;
-//    });
-//
-//    expect(localStorage.beerDesign).toBe('{"name":"Jasper","style":"Sarsaparilla","og":1.02,"ibu":10,"srm":20}');
-//  });
-//});
-//
-//it('should use localStorage value if it exists', function() {
-//  module(function($provide) {
-//    localStorage = {
-//      beerDesign: '{"name":"Jasper","style":"Sarsaparilla","og":1.02,"ibu":10,"srm":20}'
-//    };
-//
-//    $provide.value('localStorage', localStorage);
-//  });
-//
-//  inject(function(_hopStorage_) {
-//    hopStorage = _hopStorage_;
-//  });
-//
-//  expect(hopStorage.name).toBe('Jasper');
-//  expect(hopStorage.style).toBe('Sarsaparilla');
-//  expect(hopStorage.og).toBe(1.020);
-//  expect(hopStorage.ibu).toBe(10);
-//  expect(hopStorage.srm).toBe(20);
+
+  it('should persist to localStorage', function() {
+    $rootScope.$apply(function() {
+      hopStorage.hopBill = new HopBill(20);
+    });
+
+    expect(localStorage.hopStorage).toBe('{"hopBill":{"desiredIBUs":20,"bitteringHop":{},"flavorHops":[]}}');
   });
+  });
+
+/*
+  it('should use localStorage value if it exists', function () {
+    module(function ($provide) {
+      localStorage = {
+        hopBill: '{"name":"Jasper","style":"Sarsaparilla","og":1.02,"ibu":10,"srm":20}'
+      };
+
+      $provide.value('localStorage', localStorage);
+    });
+
+    inject(function (_hopStorage_) {
+      hopStorage = _hopStorage_;
+    });
+
+    expect(hopStorage.name).toBe('Jasper');
+    expect(hopStorage.style).toBe('Sarsaparilla');
+    expect(hopStorage.og).toBe(1.020);
+    expect(hopStorage.ibu).toBe(10);
+    expect(hopStorage.srm).toBe(20);
+  });
+*/
 });
